f0e69010f63b8cd65c6474be7c2de750b8095fb6
maheshpatil369 <maheshpatilmp937@gmail.com>
2025-02-14 02:42:59 +0530

    New project

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,34 @@
-import React from 'react'
-import Cards from './assets/components/Cards'
+import React, { useState } from 'react';
+import Cards from './assets/components/Cards';
 
-function App()  {
-     const data = [
-      { name: "Joohn", profession: "Painter", image:"https://plus.unsplash.com/premium_photo-1691784781482-9af9bce05096?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
-      { name: "Faltu", profession: "Sutar", image:"https://plus.unsplash.com/premium_photo-1690397038570-7ec0cacb37f2?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
-      { name: "chutiya", profession: "Paint", image:"https://plus.unsplash.com/premium_photo-1689564003745-946f35267ffe?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
-      { name: "Bulla", profession: "Carpeneter", image:"https://images.unsplash.com/photo-1509967419530-da38b4704bc6?q=80&w=2095&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
-      { name: "Rohan", profession: "Builder", image:"https://plus.unsplash.com/premium_photo-1690395794791-e85944b25c0f?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}
-     ]
+function App() {
+  const raw = [
+    { name: "John", profession: "Painter", image: "https://plus.unsplash.com/premium_photo-1691784781482-9af9bce05096?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", friends: false },
+    { name: "Faltu", profession: "Sutar", image: "https://plus.unsplash.com/premium_photo-1690397038570-7ec0cacb37f2?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", friends: false },
+    { name: "Bulla", profession: "Carpenter", image: "https://images.unsplash.com/photo-1509967419530-da38b4704bc6?q=80&w=2095&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", friends: false },
+    { name: "Rohan", profession: "Builder", image: "https://plus.unsplash.com/premium_photo-1690395794791-e85944b25c0f?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", friends: false }
+  ];
 
+  const [data, setData] = useState(raw);
+
+  const handleClick = (changingIndex) => {
+    setData((prev) => {
+      return prev.map((item, index) => {
+        if (index === changingIndex) return { ...item, friends: !item.friends };
+        return item;
+      });
+    });
+  };
 
   return (
     <>
-       <div className='w-full h-screen bg-zinc-300 flex gap-4 items-center justify-center'>
-       {data.map((item, index) => (
-  <Cards key={index} image={item.image} name={item.name} profession={item.profession} />
-))}
-
-       </div>
+      <div className="w-full h-screen bg-zinc-300 flex gap-4 items-center justify-center">
+        {data.map((item, index) => (
+          <Cards key={index} index={index} handleClick={handleClick} values={item} />
+        ))}
+      </div>
     </>
-   
-
-  
   );
 }
-export default App
+
+export default App;
--- src/assets/components/Cards.jsx
+++ src/assets/components/Cards.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
 
-function Cards({ image, name, profession}) {
+function Cards({ values, index, handleClick }) {
   return (
-    <div className='w-52 bg-white rounded-md overflow-hidden'>
-      <div className='w-full h-32 bg-sky-200 '>
-        <img className='w-full h-full object-cover' src={image} alt="" />
+    <div className="w-52 bg-white rounded-md overflow-hidden shadow-md">
+      <div className="w-full h-32 bg-sky-200">
+        <img className="w-full h-full object-cover" src={values.image} alt={values.name} />
       </div>
-        <div className='w-full p-3'>
-            <h3 className='text-xl font-semibold'>{name}</h3>
-            <h5>{Profession}</h5>
-            <button className='mt-4 px-3 py-1 text-xs text-white bg-blue-500 font-semibold rounded-md cursor-pointer'>Add Friend</button>
+      <div className="w-full p-3">
+        <h3 className="text-xl font-semibold">{values.name}</h3>
+        <h5 className="text-gray-600">{values.profession}</h5>
+        <button
+          onClick={() => handleClick(index)}
+          className={`mt-4 px-3 py-1 text-xs text-white ${
+            values.friends ? "bg-green-600" : "bg-blue-500"
+          } font-semibold rounded-md cursor-pointer`}
+        >
+          {values.friends ? "Friends" : "Add Friend"}
+        </button>
       </div>
     </div>
   );
